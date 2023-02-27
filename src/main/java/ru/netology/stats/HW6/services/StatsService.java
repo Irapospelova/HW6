148df6aa53b804659479148395e546c6975af3be
Irina Pospelova <irapospelova78@gmail.com>
2023-02-27 22:51:57 +0300

    Initial commit

---
+++ src/main/java/ru/netology/stats/HW6/services/StatsService.java
@@ -0,0 +1,95 @@
+package ru.netology.stats.HW6.services;
+
+import java.util.Arrays;
+
+public class StatsService {
+    //long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+    public int sumOfSales(long[] sales) {
+
+        long sum = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            sum += sales[i];
+        }
+
+        return (int) sum;
+
+        // System.out.println("The sum of all elements in the sales array is: " + sum);
+
+    }
+
+    public int averageSales(long[] sales) {
+        int sum = 0;
+        for (int i = 0; i < sales.length; i++) {
+            sum += sales[i];
+        }
+        int averageSales = sum / sales.length;
+
+        return averageSales;
+    }
+
+
+    public int maxSales(long[] sales) {
+
+        int maxMonth = 0; // номер последнего месяца с максимальными продажами среди просмотренных ранее
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[maxMonth]) {
+                maxMonth = i;
+            }
+        }
+
+        return maxMonth + 1;
+    }
+
+    public int minSales(long[] sales) {
+
+        int minMonth = 0; // номер месяца с минимальными продажами среди просмотренных ранее
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] < sales[minMonth]) {
+                minMonth = i;
+            }
+        }
+
+        return minMonth + 1;
+    }
+
+    public int monthsUnderAverage(long[] sales) {
+
+        int monthUnderAverage = 0;
+        int sumOfMonthsUnderAverage = 0;
+        double averageSales = 15;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] < averageSales) {
+                monthUnderAverage = 1;
+                sumOfMonthsUnderAverage += monthUnderAverage;
+            }
+        }
+        return sumOfMonthsUnderAverage;
+
+    }
+
+    public int monthsOverAverage(long[] sales) {
+
+        int monthOverAverage = 0;
+        int sumOfMonthsOverAverage = 0;
+        double averageSales = 15;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] > averageSales) {
+                monthOverAverage = 1;
+                sumOfMonthsOverAverage += monthOverAverage;
+            }
+        }
+        return sumOfMonthsOverAverage;
+
+    }
+}
+
+
+
+
+
---
+++ src/test/java/ru/netology/stats/HW6/services/StatsServiceTest.java
@@ -0,0 +1,81 @@
+package ru.netology.stats.HW6.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatsServiceTest {
+
+    @Test
+    public void shouldCalcSumOfSales() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expectedSum = 180;
+        int actualSum = service.sumOfSales(sales);
+
+        Assertions.assertEquals(expectedSum, actualSum);
+    }
+
+    @Test
+    public void shouldCalcAverageSales() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expectedAverage = 15;
+        int actualAverage = service.averageSales(sales);
+
+        Assertions.assertEquals(expectedAverage, actualAverage);
+
+    }
+
+    @Test
+    public void shouldFindMaxMonth() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expectedMonth = 8;
+        int actualMonth = service.maxSales(sales);
+
+        Assertions.assertEquals(expectedMonth, actualMonth);
+
+    }
+
+    @Test
+    public void shouldFindMinMonth() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expectedMonth = 9;
+        int actualMonth = service.minSales(sales);
+
+        Assertions.assertEquals(expectedMonth, actualMonth);
+
+
+    }
+
+    @Test
+    public void shouldCalcSumOfMonthsUnderAverage() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expectedSumOfMonthsUnderAverage = 5;
+        int actualSumOfMonthsUnderAverage = service.monthsUnderAverage(sales);
+
+        Assertions.assertEquals(expectedSumOfMonthsUnderAverage, actualSumOfMonthsUnderAverage);
+
+    }
+
+    @Test
+    public void shouldCalculateSumOfMonthsOverAverage() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expectedSumOfMonthsOverAverage = 5;
+        int actualSumOfMonthsOverAverage = service.monthsOverAverage(sales);
+
+        Assertions.assertEquals(expectedSumOfMonthsOverAverage, actualSumOfMonthsOverAverage);
+
+    }
+}
+
+
